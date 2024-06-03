22edcf08acc2cc7ba890721d0f368f3c2733bb14
Alessandro Bollini <alessandro.bollini@metreeca.com>
2024-06-03 09:36:24 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,37 @@
+This action mirrors the triggering branch to a target branch.
+
+# Usage
+
+## Inputs
+
+| name     | default               | description                  |
+|----------|-----------------------|------------------------------|
+| `token`  | `${{ github.token }}` | GitHub API access token      |
+| `branch` | *required*            | name of the mirroring branch |
+
+## Example
+
+Mirror the `release/v1.0` branch to the `main` branch:
+
+```yaml
+name: Mirror branch
+
+on:
+  push:
+    branches: release/v1.0
+
+jobs:
+  mirror-to-main:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: metreeca/gh-mirror-action@v1
+```
+
+# Support
+
+- open an [issue](ssues) to report a problem or to suggest a new feature
+- start a [discussion](discussions) to ask a how-to question or to share an idea
+
+# License
+
+This project is licensed under the Apache 2.0 License – see [LICENSE](LICENSE) file for details.
---
+++ index.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2024 Metreeca srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+const token=core.getInput("token", { required: true });
+const branch=core.getInput("branch", { required: true });
+
+const octokit=github.getOctokit(token);
+
+const { repo: { owner, repo }, sha }=github.context;
+
+octokit.git.updateRef({
+
+	owner,
+	repo,
+
+	ref: `heads/${branch}`,
+	sha,
+
+	force: true
+
+}).catch((error: unknown) => {
+
+	core.setFailed(`failed to update ref: ${error}`);
+
+});
